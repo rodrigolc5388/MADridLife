bc101d97b4ec147a6e70865c54c1940a87bd76b5
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-09-22 09:28:10 +0200

    Initial commit. Blank app

---
+++ MADridLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MADridLife
+//
+//  Created by Rodrigo Limpias Cossio on 21/9/17.
+//  Copyright © 2017 Rodrigo Limpias Cossio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
