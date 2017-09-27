d56d086a03e0075d13ae9e63816b0c8ae3488034
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-09-27 16:31:40 +0200

    SaveShoptivitiesIntImpl created; MainVC implements segues to sections

---
+++ MADridLife/Models/Interactors/SaveShoptivitiesInteractorImpl.swift
@@ -0,0 +1,9 @@
+//
+//  SaveShoptivitiesInteractorImpl.swift
+//  MADridLife
+//
+//  Created by Rodrigo Limpias Cossio on 27/9/17.
+//  Copyright © 2017 Rodrigo Limpias Cossio. All rights reserved.
+//
+
+import Foundation
